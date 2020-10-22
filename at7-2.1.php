6adf4aeeb4da0b98e23e74a6bbe4871cfe9e6a31
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-22 11:38:45 +0100

    Update at7-2.1.php, at7-2.php, and 2 more files...

--- at7-2.1.php
+++ at7-2.1.php
@@ -1,16 +1,18 @@
 <?php
-$nota=12;
-if($nota<10)
+$nota=0;
+if($nota<20)
 {
     echo 'Aluno <span style="color:#ff00000">REPROVADO</span>com'
 .$nota. 'valores. ';
 echo '<br>Deverá trabalhar mais para alcançar resultados postivos';
-}
-elseif($nota>=10 && $nota<=20){
+switch ( true ){
+case($nota>=10 && $nota<=20):
     echo 'Aluno <span style="color:#00ff00">APROVADO</span> com' .$nota. 'valores.';
     echo '<br>Muito bom trabalho';
-}
-elseif($nota>20 || $nota<0){
+        break;
+case($nota>20 || $nota<0):
         echo '"Valor da nota não é válida"';
+        break;
     }
+}
 ?>
---
+++ at7-3.php
@@ -0,0 +1,28 @@
+<?php
+    $diaSemana =5;
+switch ($diaSemana){
+    case 1:
+        print('Domingo');
+        break;
+    case 2:
+        print('Segunda-Feira');
+        break;
+    case 3:
+        print ('Terça-Feira');
+        break;
+    case 4:
+        print('Quarta-Feira');
+            break;
+    case 5:
+        print('Quinta-Feira');
+        break;
+    case 6:
+        print('Sexta-Feira');
+        break;
+    case 7:
+        print('Sabado');
+        break;
+    default:
+        echo 'Erro ao avaliar o dia da semana';
+}
+?>
